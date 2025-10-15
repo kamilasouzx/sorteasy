d681ff7801702fe8fec0f12d7b5da4c17083a7f0
kamila_souza-silva <kskamila39@gmail.com>
2025-10-15 17:19:37 -0300

    first commit

---
+++ sorteasy/src/main/java/com/sorteasy/sorteasy/SorteasyApplication.java
@@ -0,0 +1,13 @@
+package com.sorteasy.sorteasy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SorteasyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SorteasyApplication.class, args);
+	}
+
+}
---
+++ sorteasy/src/test/java/com/sorteasy/sorteasy/SorteasyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sorteasy.sorteasy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SorteasyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
